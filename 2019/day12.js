e009b38defbfb56d08e3b87b1afce72fe573c0d4
emrergin <emrergin2757@yahoo.com>
2022-10-04 11:56:23 +0300

    2021 day3

--- 2019/day12.js
+++ 2019/day12.js
@@ -3,7 +3,7 @@ const europa={x:-9, y:3, z:17, vel:{x:0,y:0,z:0}};
 const ganymede={x:9, y:-4, z:14, vel:{x:0,y:0,z:0}};
 const callisto={x:4, y:14, z:4, vel:{x:0,y:0,z:0}};
 
-const jupitersMoons = [io,europa,ganymede,callisto];
+const jupitersMoons = [{...io,initial:io},{...europa,initial:europa},{...ganymede,initial:ganymede},{...callisto,initial:callisto}];
 
 const io2 ={x:-8, y:-10, z:0, vel:{x:0,y:0,z:0}};
 const europa2={x:5, y:5, z:10, vel:{x:0,y:0,z:0}};
@@ -12,12 +12,12 @@ const callisto2={x:9, y:-8, z:-3, vel:{x:0,y:0,z:0}};
 
 const jupitersMoons2 = [io2,europa2,ganymede2,callisto2];
 
-const io3 ={x:-1, y:0, z:2, vel:{x:0,y:0,z:0}};
-const europa3={x:2, y:-10, z:-7, vel:{x:0,y:0,z:0}};
-const ganymede3={x:4, y:-8, z:8, vel:{x:0,y:0,z:0}};
-const callisto3={x:3, y:5, z:-1, vel:{x:0,y:0,z:0}};
+const io3 ={x:-1, y:0, z:2, vel:{x:0,y:0,z:0}, name:'io'};
+const europa3={x:2, y:-10, z:-7, vel:{x:0,y:0,z:0}, name:'europa'};
+const ganymede3={x:4, y:-8, z:8, vel:{x:0,y:0,z:0}, name:'ganymede'};
+const callisto3={x:3, y:5, z:-1, vel:{x:0,y:0,z:0}, name:'callisto'};
 
-const jupitersMoons3 = [io3,europa3,ganymede3,callisto3];
+const jupitersMoons3 = [{...io3,initial:io3},{...europa3,initial:europa3},{...ganymede3,initial:ganymede3},{...callisto3,initial:callisto3}];
 
 function findGravities(moons){
     for(let i=0;i<moons.length;i++){
@@ -48,17 +48,58 @@ function findEnergies(moons){
     }
 }
 function resolveCycles(k,moons){
+    // let cycles=[null,null,null,null]
+    let cycles=[null,null,null];
     for( let steps=0;steps<k;steps++){
-        console.log(steps,moons);
         findGravities(moons);
         resolveMovement(moons);
-        console.log(steps,moons);
+        // for(let i=0;i<4;i++){
+        //     if (moons[i].vel.x===0 && moons[i].vel.y===0 && moons[i].vel.z===0 && moons[i].cycleLength===undefined
+        //         &&  moons[i].x===moons[i].initial.x && moons[i].y===moons[i].initial.y && moons[i].z===moons[i].initial.z){
+        //         moons[i].cycleLength=steps+1;
+        //     }
+        // }
+        // if (moons.filter(moon=>moon.cycleLength===undefined).length===0){
+        //     break;
+        // }
+        if(moons[0].x===moons[0].initial.x && moons[1].x===moons[1].initial.x &&  moons[2].x===moons[2].initial.x  && moons[3].x===moons[3].initial.x && cycles[0]===null){
+            cycles[0]=steps+1;
+            console.log(cycles);
+        }
+        if(moons[0].y===moons[0].initial.y && moons[1].y===moons[1].initial.y &&  moons[2].y===moons[2].initial.y  && moons[3].y===moons[3].initial.y && cycles[1]===null){
+            cycles[1]=steps+1;
+            console.log(cycles);
+        }
+        if(moons[0].z===moons[0].initial.z && moons[1].z===moons[1].initial.z &&  moons[2].z===moons[2].initial.z  && moons[3].z===moons[3].initial.z && cycles[2]===null){
+            cycles[2]=steps+1;
+            console.log(cycles);
+        }
+
+        if(cycles.filter(a=>a===null).length===0){break;}
+        
     }
-    findEnergies(moons);
+    console.log(moons)
+    // console.log(moons.map(a=>a.cycleLength).reduce((prev,curr)=>lcm_two_numbers(prev,curr),1));
+    // console.log(cycles.reduce((prev,curr)=>lcm_two_numbers(prev,curr),1));
+    // console.log(cycles)
 }
 
-resolveCycles(1000,jupitersMoons);
-// console.log(jupitersMoons);
-// console.log(jupitersMoons.reduce((prev,curr)=>prev+curr.total,0))
-console.log(jupitersMoons);
-console.log(jupitersMoons.reduce((prev,curr)=>prev+curr.total,0))
+resolveCycles(Infinity,jupitersMoons3);
+
+// https://www.w3resource.com/javascript-exercises/javascript-math-exercise-10.php
+function lcm_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+     return false;
+   return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+ }
+ 
+ function gcd_two_numbers(x, y) {
+   x = Math.abs(x);
+   y = Math.abs(y);
+   while(y) {
+     var t = y;
+     y = x % y;
+     x = t;
+   }
+   return x;
+ }
--- 2021/day3.js
+++ 2021/day3.js
@@ -1,27 +1,96 @@
 import day3input from "./day3input.mjs";
 
-let resultArray = Array(12).fill().map(a=>(Object.assign({}, {zeros:0,ones:0})))
+let test = [
+"00100",
+"11110",
+"10110",
+"10111",
+"10101",
+"01111",
+"00111",
+"11100",
+"10000",
+"11001",
+"00010",
+"01010"
+]
 
-let parsedInput=day3input.map(a=>a.split("")).reduce(
-    (prev,thisNumberArray)=>{
-        thisNumberArray.forEach((value,index) =>{
-            if (value===`1`){
-                prev[index].ones++;
-            }else{
-                prev[index].zeros++;
-            }   
+function powerConsumption(){
+    // Related Links
+    // https://stackoverflow.com/questions/35578478/array-prototype-fill-with-object-passes-reference-and-not-new-instance
+    let resultArray = Array(12).fill().map(a=>(Object.assign({}, {zeros:0,ones:0})))
+
+    let parsedInput=day3input.map(a=>a.split("")).reduce(
+        (prev,thisNumberArray)=>{
+            thisNumberArray.forEach((value,index) =>{
+                if (value===`1`){
+                    prev[index].ones++;
+                }else{
+                    prev[index].zeros++;
+                }   
+            }
+            )
+            return prev
         }
-        )
-        return prev
+        ,resultArray
+    )
+
+    const gamma = parseInt(parsedInput.map(a=> a.zeros>a.ones ? 0 : 1).join(''),2)
+    const epsilon =  parseInt(parsedInput.map(a=> a.zeros>a.ones ? 1 : 0).join(''),2)
+
+    console.log(gamma*epsilon)
+}
+
+function parseOnesZeros(arr,priority=0){
+    let resultArray = Array(arr[0].length).fill().map(a=>(Object.assign({}, {zeros:0,ones:0})))
+    let arrayOfObjects=  arr.reduce(
+        (prev,thisNumberArray)=>{
+            thisNumberArray.forEach((value,index) =>{
+                if (value===`1`){
+                    prev[index].ones++;
+                }else{
+                    prev[index].zeros++;
+                }   
+            }
+            )
+            return prev
+        }
+        ,resultArray
+    )
+    if (priority===0){
+        return arrayOfObjects.map(a=>a.zeros>=a.ones? `0`:`1`);
     }
-    ,resultArray
-)
+    else{
+        return arrayOfObjects.map(a=>a.zeros>a.ones? `0`:`1`);
+    }    
+}
 
+function lifeSupport(arrayToUse){
+    // let arrayToUse = arrayToUse.map(a=>a.split(""))
+    let parsedInput=arrayToUse.map(a=>a.split(""));    
+    
+    function O2(input){        
+        let index=0;
+        let onesZeros = parseOnesZeros(input,1);
+        while(input.length>1){
+            input=input.filter(a=>a[index]===onesZeros[index]);
+            onesZeros = parseOnesZeros(input,1);
+            index++;
+        }
+        return(parseInt(input[0].join(''),2));
+    }
 
-const gamma = parseInt(parsedInput.map(a=> a.zeros>a.ones ? 0 : 1).join(''),2)
-const epsilon =  parseInt(parsedInput.map(a=> a.zeros>a.ones ? 1 : 0).join(''),2)
+    function CO2(input){        
+        let index=0;
+        let onesZeros = parseOnesZeros(input,1);
+        while(input.length>1){
+            input=input.filter(a=>a[index]!==onesZeros[index]);
+            onesZeros = parseOnesZeros(input,1);
+            index++;
+        }
+        return(parseInt(input[0].join(''),2));
+    }
 
-console.log(gamma*epsilon)
-
-// Related Links
-// https://stackoverflow.com/questions/35578478/array-prototype-fill-with-object-passes-reference-and-not-new-instance
+    console.log(O2([...parsedInput])*CO2([...parsedInput]))
+}
+lifeSupport(day3input);
