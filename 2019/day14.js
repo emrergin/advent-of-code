dace6f7ce6b4c5334a888d382f8bd553278dfe6e
emrergin <emrergin2757@yahoo.com>
2022-12-02 17:33:29 +0300

    2019d14p1

--- 2019/day14.js
+++ 2019/day14.js
@@ -1,6 +1,6 @@
 import {readFileSync } from "fs";
 
-let lines = readFileSync(`day14test1.txt`, 'utf-8')
+let lines = readFileSync(`day14input.txt`, 'utf-8')
                 .split(`\n`).map(a=>a.split('=>'))
                 .map(a=>a.flatMap(b=>b.trim().split(',')))
                 .map(a=>a.map(b=>b.trim().split(" "))
@@ -16,15 +16,14 @@ let lines = readFileSync(`day14test1.txt`, 'utf-8')
                 );
 
 function findInputs(element){
-    let finalInput = lines.find(a=>a.output[1]===element).input;
+    let finalInput = structuredClone(lines.find(a=>a.output[1]===element).input);
     
     while(finalInput.filter(a=>a[0]>0).length>1){ 
-        const currentLength = finalInput.length;
-        for(let i=0; i<currentLength;i++){
-            console.log(finalInput);
+        for(let i=0; i<finalInput.length;i++){
             
             if(finalInput[i][1]!=="ORE"&&finalInput[i][0]>0){
-                const relatedProcess = lines.find(a=>a.output[1]===finalInput[i][1]);
+                const relatedProcess = structuredClone(lines.find(a=>a.output[1]===finalInput[i][1]));
+                // console.log(relatedProcess)
                 finalInput[i][0]-=relatedProcess.output[0];
                 finalInput = finalInput.concat(relatedProcess.input);
                 finalInput= finalInput.reduce((acc,curr)=>{
@@ -36,11 +35,9 @@ function findInputs(element){
                 finalInput=finalInput.filter(a=>a[0]!==0);
             }
         }
-        // is++;
-        // console.log(finalInput);
        }
 
-    console.log(finalInput)
+    console.log(finalInput);
 }
 
 findInputs("FUEL");
