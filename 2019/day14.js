ceee7b0d06ba0630b089521b6ec9d22c52799671
emrergin <emrergin2757@yahoo.com>
2022-12-03 11:36:25 +0300

    2022d3

--- 2019/day14.js
+++ 2019/day14.js
@@ -1,6 +1,6 @@
 import {readFileSync } from "fs";
 
-let lines = readFileSync(`day14input.txt`, 'utf-8')
+let lines = readFileSync(`day14test3.txt`, 'utf-8')
                 .split(`\n`).map(a=>a.split('=>'))
                 .map(a=>a.flatMap(b=>b.trim().split(',')))
                 .map(a=>a.map(b=>b.trim().split(" "))
@@ -15,29 +15,80 @@ let lines = readFileSync(`day14input.txt`, 'utf-8')
                     },{input:[],output:[]})
                 );
 
-function findInputs(element){
-    let finalInput = structuredClone(lines.find(a=>a.output[1]===element).input);
-    
-    while(finalInput.filter(a=>a[0]>0).length>1){ 
-        for(let i=0; i<finalInput.length;i++){
+function cleanUp(arrayOfInputs){
+    return arrayOfInputs.reduce((acc,curr)=>{
+        let cellToAdd=acc.findIndex(a=>a[1]===curr[1]);
+        if(cellToAdd!==-1){acc[cellToAdd][0]+=curr[0];}
+        else{acc.push(curr);}
+        return acc;
+    },[]);
+}
+
+function produceOneFuel(currentInput){
+    while(currentInput.filter(a=>a[0]>0).length>1){ 
+        for(let i=0; i<currentInput.length;i++){
             
-            if(finalInput[i][1]!=="ORE"&&finalInput[i][0]>0){
-                const relatedProcess = structuredClone(lines.find(a=>a.output[1]===finalInput[i][1]));
-                // console.log(relatedProcess)
-                finalInput[i][0]-=relatedProcess.output[0];
-                finalInput = finalInput.concat(relatedProcess.input);
-                finalInput= finalInput.reduce((acc,curr)=>{
-                    let cellToAdd=acc.findIndex(a=>a[1]===curr[1]);
-                    if(cellToAdd!==-1){acc[cellToAdd][0]+=curr[0];}
-                    else{acc.push(curr);}
-                    return acc;
-                },[])
-                finalInput=finalInput.filter(a=>a[0]!==0);
+            if(currentInput[i][1]!=="ORE"&&currentInput[i][0]>0){
+                const relatedProcess = structuredClone(lines.find(a=>a.output[1]===currentInput[i][1]));
+                currentInput[i][0]-=relatedProcess.output[0];
+                currentInput = currentInput.concat(relatedProcess.input);
+                currentInput= cleanUp(currentInput); 
+                currentInput=currentInput.filter(a=>a[0]!==0);
             }
         }
-       }
+    }
+    return currentInput;
+}
+// Not complete yet.
+function produceFuel(currentInput){
+    while(currentInput.filter(a=>a[0]>0).length>1){ 
+        for(let i=0; i<currentInput.length;i++){
+            if(currentInput[i][1]!=="ORE"&&currentInput[i][0]>0){
+                const relatedProcess = structuredClone(lines.find(a=>a.output[1]===currentInput[i][1]));
+                const timesToProcess = Math.max(Math.ceil(relatedProcess.output[0]/currentInput[i][0]),1);
+                currentInput[i][0]-=(timesToProcess*relatedProcess.output[0]);
+                currentInput = currentInput.concat(relatedProcess.input.map(a=>[a[0]*timesToProcess,a[1]]));
+                currentInput= cleanUp(currentInput); 
+                currentInput=currentInput.filter(a=>a[0]!==0);
+            }
+        }
+    }
+    return currentInput;
+}
 
+function partOne(){
+    let finalInput = structuredClone(lines.find(a=>a.output[1]==="FUEL").input);
+    finalInput=produceOneFuel(finalInput);
     console.log(finalInput);
 }
 
-findInputs("FUEL");
+function partOnePrime(){
+    let finalInput = structuredClone(lines.find(a=>a.output[1]==="FUEL").input);
+    finalInput=produceFuel(finalInput);
+    console.log(finalInput);
+}
+
+function partTwo(){
+    let finalInput = structuredClone(lines.find(a=>a.output[1]==="FUEL").input);
+    let currentFuel=0;
+    
+    while(true){        
+        finalInput=produceFuel(finalInput);
+        const newFuelToAdd=structuredClone(lines.find(a=>a.output[1]==="FUEL").input);
+        finalInput = finalInput.concat(newFuelToAdd);
+        currentFuel++;
+        finalInput= cleanUp(finalInput); 
+        if(finalInput.find(a=>a[1]==="ORE")&& finalInput.find(a=>a[1]==="ORE")[0]>1000000000000){break;}
+        if(finalInput.length===1){break;}
+        if(currentFuel%1000===0){console.log(currentFuel)}
+    }
+
+    console.log(currentFuel-1,finalInput);
+}
+
+
+// function addNewFuelToProduce()
+partOne();
+partOnePrime();
+// partTwo();
+// findInputs("FUEL");
