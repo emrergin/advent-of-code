7944429f0165bd0e39f7652efb2f8efaa71d2ae1
emrergin <emrergin2757@yahoo.com>
2022-12-03 23:45:54 +0300

    2020,d1,d2

---
+++ 2020/day1.js
@@ -0,0 +1,32 @@
+import {readFileSync } from "fs";
+
+let lines = readFileSync(`day1input.txt`, 'utf-8')
+                .split(`\n`).map(a=>+a);
+
+const skimmed = new Map();
+
+function partOne(){
+    for (let line of lines){
+        if(skimmed.get(2020-line)){
+            console.log(line*(2020-line));
+            break;
+        }
+        else{
+            skimmed.set(line,true);
+        }
+    }
+}
+
+function partTwo(){
+    outerloop: for (let line1 of lines){
+        for (let line2 of lines){
+            if(skimmed.get(2020-line1-line2)){
+                console.log(line1*line2*(2020-line1-line2));
+                break outerloop;
+            }
+            else{
+                skimmed.set(line2,true);
+            }
+        }
+    }
+}
---
+++ 2020/day2.js
@@ -0,0 +1,41 @@
+import {readFileSync } from "fs";
+
+let lines = readFileSync(`day2input.txt`, 'utf-8')
+                .split(`\n`).map(l=>l.split(" "))
+                .map(a=>[a[0].split("-"),a[1][0],a[2]])
+                .map(a=>({min:+a[0][0], max:+a[0][1], letter:a[1], password:a[2]}));
+
+function checkPassword1(line){
+    let countLetter=0;
+    for(let i=0;i<line.password.length;i++){
+        if(line.password[i]===line.letter){
+            countLetter++;
+        }
+    }
+    return (countLetter<=line.max)&&(countLetter>=line.min);
+}
+
+function checkPassword2(line){
+    return (((line.password[line.min-1]===line.letter)||(line.password[line.max-1]===line.letter))&&
+            ((line.password[line.min-1]!==line.letter)||(line.password[line.max-1]!==line.letter)))  
+}
+
+function partOne(){
+    let finalCounter=0;
+    lines.forEach(a=>{
+        if(checkPassword1(a)){
+            finalCounter++;
+        }
+    })
+    console.log(finalCounter);
+}
+
+function partTwo(){
+    let finalCounter=0;
+    lines.forEach(a=>{
+        if(checkPassword2(a)){
+            finalCounter++;
+        }
+    })
+    console.log(finalCounter);
+}
