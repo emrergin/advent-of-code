82a86f69d5576f1197f78d2a2ded11649cd914ac
emrergin <emrergin2757@yahoo.com>
2022-12-06 11:07:19 +0300

    some code cleanup for 2022d6

--- 2022/day6.js
+++ 2022/day6.js
@@ -4,26 +4,23 @@ let letters = readFileSync(`day6input.txt`, 'utf-8').split("");
 
 function partOne(){    
     for(let i=0;i<letters.length;i++){
-        if(i>2){
-            let lastFour = new Set(letters.slice(i-3,i+1));
-            if (lastFour.size===4){
-                console.log(i+1);
-                break;
-            }
+        let lastFour = new Set(letters.slice(i-4,i));
+        if (lastFour.size===4){
+            console.log(i);
+            break;
         }
     }
 }
 
 function partTwo(){
     for(let i=0;i<letters.length;i++){
-        if(i>12){
-            let lastFour = new Set(letters.slice(i-13,i+1));
-            if (lastFour.size===14){
-                console.log(i+1);
-                break;
-            }
+        let lastFour = new Set(letters.slice(i-14,i));
+        if (lastFour.size===14){
+            console.log(i);
+            break;
         }
     }
 }
+
 partOne();
 partTwo();
