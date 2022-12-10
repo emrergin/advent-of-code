97f302586d34e1be2f5ebd0186757b02d949998e
emrergin <emrergin2757@yahoo.com>
2022-12-10 11:46:33 +0300

    some cleanup

--- 2022/day10.js
+++ 2022/day10.js
@@ -4,7 +4,7 @@ const commands = readFileSync(`day10input.txt`, 'utf-8')
     .map(a=>a.split(" "))
     .map(a=>[a[0],a[1]? +a[1]:null])
 
-const xCoordinates = commands.reduce((acc,curr)=>{
+const xCommands = commands.reduce((acc,curr)=>{
     acc.push(0);
     if(curr[1]){
         acc.push(curr[1]);
@@ -13,13 +13,10 @@ const xCoordinates = commands.reduce((acc,curr)=>{
 },[1]);
 
 function partOne(){
-    const signal1 = (xCoordinates.slice(0,20).reduce((acc,curr)=>acc+curr,0))*20;
-    const signal2 = (xCoordinates.slice(0,60).reduce((acc,curr)=>acc+curr,0))*60;
-    const signal3 = (xCoordinates.slice(0,100).reduce((acc,curr)=>acc+curr,0))*100;
-    const signal4 = (xCoordinates.slice(0,140).reduce((acc,curr)=>acc+curr,0))*140;
-    const signal5 = (xCoordinates.slice(0,180).reduce((acc,curr)=>acc+curr,0))*180;
-    const signal6 = (xCoordinates.slice(0,220).reduce((acc,curr)=>acc+curr,0))*220;
-    console.log(signal1+signal2+signal3+signal4+signal5+signal6);
+    function getSignal(upTo){
+        return (xCommands.slice(0,upTo).reduce((acc,curr)=>acc+curr,0))*upTo;
+    }
+    console.log([20,60,100,140,180,220].map(getSignal).reduce((acc,curr)=>acc+curr,0));
 }
 
 function partTwo(){
@@ -27,7 +24,7 @@ function partTwo(){
     for(let i=0;i<6;i++){
         let row='';
         for(let j=0;j<40;j++){
-            xregister+=xCoordinates[i*40+j];
+            xregister+=xCommands[i*40+j];
             if(xregister-j<2&&xregister-j>-2){
                 row+='⬛';
             }else{
