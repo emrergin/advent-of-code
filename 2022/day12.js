54594bd214858ef41d122847b32b9b01352459c0
emrergin <emrergin2757@yahoo.com>
2022-12-12 13:10:18 +0300

    optimization for 2022d12p2

--- 2022/day12.js
+++ 2022/day12.js
@@ -27,22 +27,42 @@ function controlNeighbourToAdd(xx,yy,currentLetter){
     if(targetLetter==="E"){
         return false;
     }
-    if(targetLetter.charCodeAt(0)-currentLetter.charCodeAt(0)>1){
+
+    if(convertToPriority(targetLetter)-convertToPriority(currentLetter)>1){   
         return false;
     }
     return true;
 }
 
-function findDistanceFrom(tarX,tarY){
+function controlNeighbourToAdd2(xx,yy,currentLetter){
+    if(xx>=numX){return false;}
+    if(xx<0){return false;}
+    if(yy>=numY){return false;}
+    if(yy<0){return false;}
+    const targetLetter = commands[yy][xx];
+
+    if(convertToPriority(currentLetter)-convertToPriority(targetLetter)>1){        
+        return false;
+    }
+    return true;
+}
+
+function convertToPriority(letter){
+    if(letter==="E"){return "{".charCodeAt(0);}
+    if(letter==="S"){return "a".charCodeAt(0);}
+    return letter.charCodeAt(0);
+}
+
+function findDistanceFrom(tarX,tarY,controlFunction,part){
     const tileMap = new Map();
 
     for (let y=0;y<commands.length;y++){
         for (let x=0;x<commands[0].length;x++){
             const tile = new Tile(commands[y][x],x,y);
-            controlNeighbourToAdd(x+1,y,commands[y][x]) && tile.neighbours.push(`x:${x+1}-y:${y}`);
-            controlNeighbourToAdd(x-1,y,commands[y][x])&& tile.neighbours.push(`x:${x-1}-y:${y}`);
-            controlNeighbourToAdd(x,y+1,commands[y][x]) && tile.neighbours.push(`x:${x}-y:${y+1}`);
-            controlNeighbourToAdd(x,y-1,commands[y][x]) && tile.neighbours.push(`x:${x}-y:${y-1}`);
+            controlFunction(x+1,y,commands[y][x]) && tile.neighbours.push(`x:${x+1}-y:${y}`);
+            controlFunction(x-1,y,commands[y][x])&& tile.neighbours.push(`x:${x-1}-y:${y}`);
+            controlFunction(x,y+1,commands[y][x]) && tile.neighbours.push(`x:${x}-y:${y+1}`);
+            controlFunction(x,y-1,commands[y][x]) && tile.neighbours.push(`x:${x}-y:${y-1}`);
             tileMap.set(`x:${x}-y:${y}`,tile);
         }
     }
@@ -59,47 +79,46 @@ function findDistanceFrom(tarX,tarY){
                 w.explored=true;
                 w.distance=v.distance+1;
                 queue.push(w);
+                if(w.letter==="E" && part===1){
+                    return w.distance;
+                }
+                if((w.letter==="a" || w.letter==="S") && part===2){
+                    return w.distance;
+                }
             }
         }
     }
-    
-    let resultingDistance;
-    tileMap.forEach((v)=>{
-        if(v.letter==="E"){
-            resultingDistance= v.distance;
-        }
-    })
-    return resultingDistance;
 }
 
 function partOne(){
-    const startingPoints=[];
-    for (let y=0;y<commands.length;y++){
+    let startingPoint;
+    outerloop: for (let y=0;y<commands.length;y++){
         for (let x=0;x<commands[0].length;x++){
             if(commands[y][x]==="S"){
-                startingPoints.push({x,y});
+                startingPoint={x,y};
+                break outerloop;
             }
         }
     }
 
-    const result = startingPoints.map(a=>findDistanceFrom(a.x,a.y)).sort((a,b)=>a-b)[0];
+    const result = findDistanceFrom(startingPoint.x,startingPoint.y,controlNeighbourToAdd,1);
     console.log(result);
 }
 
 function partTwo(){
-    const startingPoints=[];
-    for (let y=0;y<commands.length;y++){
+    let startingPoint;
+    outerloop: for (let y=0;y<commands.length;y++){
         for (let x=0;x<commands[0].length;x++){
-            if(commands[y][x]==="S" || commands[y][x]==="a"){
-                startingPoints.push({x,y});
+            if(commands[y][x]==="E"){
+                startingPoint={x,y};
+                break outerloop;
             }
         }
     }
     
-    const result = startingPoints.map(a=>findDistanceFrom(a.x,a.y)).sort((a,b)=>a-b)[0];
-    console.log(result);    
+    const result = findDistanceFrom(startingPoint.x,startingPoint.y,controlNeighbourToAdd2,2);
+    console.log(result);   
 }
 
 partOne();
 partTwo();
-
