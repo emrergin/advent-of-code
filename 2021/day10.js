b513340b57ff81f7b5f91acd66767b8923754c92
emrergin <emrergin2757@yahoo.com>
2023-04-25 11:48:50 +0300

    2021 d10 p2

--- 2021/day10.js
+++ 2021/day10.js
@@ -11,6 +11,13 @@ const scores ={
     ")":3
 }
 
+const scores2 ={
+    "{":3,
+    "[":2,
+    "<":4,
+    "(":1
+}
+
 function removeBrackets(string){
     const newString = string.replaceAll(/(\[\])|(\(\))|(\<\>)|(\{\})/g,"")
     if(newString===string){
@@ -25,8 +32,15 @@ function calculateScore1(string){
     const cleanedString = removeBrackets(string);
     var m = cleanedString.match(/(\])|(\))|(\})|(\>)/);
     if (m===null){return 0;}
-    console.log(m[0][m[0].length-1],scores[m[0][m[0].length-1]])
     return scores[m[0][m[0].length-1]];
+}
+
+function calculateScore2(string){
+    const cleanedString = removeBrackets(string);
+    const score1 = calculateScore1(cleanedString);
+    if (score1!==0){return 0;}
+    const score2 = cleanedString.split("").reverse().reduce((acc,curr)=>acc*5+scores2[curr],0);
+    return score2;
 }
 
 
@@ -38,4 +52,14 @@ function partOne() {
     console.log(counter);
 }
 
+function partTwo() {
+    let scores = [];
+    for (let i = 0; i < lines.length; i++) {
+        scores.push(calculateScore2(lines[i]));
+    }
+    scores = scores.filter(a=>a!==0).sort((a,b)=>b-a);
+    console.log(scores[((scores.length+1)/2)-1]);
+}
+
 partOne();
+partTwo();
