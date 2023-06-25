ca545d7e91b17ff52a11031b386e3c26dbd80ba1
Emre Ergin <emrergin2757@yahoo.com>
2023-06-25 23:14:53 +0300

    2019 d13 not working but cool implementation

--- 2019/day13rewrite.js
+++ 2019/day13rewrite.js
@@ -5,43 +5,75 @@ const nums = readFileSync(`day13input.txt`, "utf-8").split(",");
 export class ArcadeMachine extends Computer {
   constructor(opcode, inputs = [], relativeBase = 0) {
     super(opcode, inputs, relativeBase);
-    this.joysticklocation = 18;
   }
 
-  workOnce(touch) {
-    super.workOnce();
-    if (touch) {
-      if (this.outputs[this.outputs.length - 3] === "-1") {
-        console.log(this.outputs[this.outputs.length - 1]);
-      }
+  get numberOfBlocks() {
+    return this.outputs.filter((a, index) => index % 3 == 2 && a === "2")
+      .length;
+  }
 
-      if (this.outputs[this.outputs.length - 1] === "4") {
-        console.log("uhhhh");
-        if (this.outputs[this.outputs.length - 3] < this.joysticklocation) {
-          this.inputs.push("-1");
-        } else if (
-          this.outputs[this.outputs.length - 3] > this.joysticklocation
-        ) {
-          this.inputs.push("1");
-        } else {
-          this.inputs.push("0");
-        }
+  positionOfBall() {
+    console.log(this.outputs);
+    return this.outputs.find(
+      (a, index) => index % 3 === 0 && this.outputs[index + 2] === "4"
+    );
+  }
+
+  newInput() {
+    const [paddlePositionLeft, paddlePositionRight] = this.paddlePosition;
+    const currentBallPosition = this.positionOfBall();
+    if (+paddlePositionLeft > +currentBallPosition) {
+      this.inputs.push("-1");
+    } else if (+paddlePositionRight < +currentBallPosition) {
+      this.inputs.push("1");
+    } else {
+      this.inputs.push("0");
+    }
+  }
+
+  get paddlePosition() {
+    const leftEdge =
+      this.outputs.findIndex((a, index) => index % 3 === 2 && a === "3") - 2;
+    const rightEdge =
+      this.outputs.findLastIndex((a, index) => index % 3 === 2 && a === "3") -
+      2;
+    return [this.outputs[leftEdge], this.outputs[rightEdge]];
+  }
+
+  get score() {
+    const indexOfScore =
+      this.outputs.findIndex((a, index) => index % 3 === 0 && a === "-1") + 2;
+    return this.outputs[indexOfScore];
+  }
+
+  workTillEnd() {
+    while (!this.halted) {
+      let currentInputSize = this.inputs.length;
+      super.workOnce();
+      let updatedInputSize = this.inputs.length;
+      if (updatedInputSize > currentInputSize) {
+        this.inputs.pop();
+        this.newInput();
       }
     }
+    return this.score;
   }
 }
 
 function partOne() {
   const machine = new ArcadeMachine(nums, ["2"]);
   machine.workTillEnd();
-  console.log(
-    machine.outputs.filter((a, index) => index % 3 == 2 && a === "2").length
-  );
+  console.log(machine.numberOfBlocks);
 }
 
 function partTwo() {
   const machine = new ArcadeMachine(nums, ["2"]);
-  machine.workTillEnd(true);
+  let score;
+  while (machine.numberOfBlocks > 0) {
+    score = machine.workTillEnd() || score;
+  }
+  console.log(score, machine.numberOfBlocks);
+  console.log(machine.paddlePosition);
 }
 
 partOne();
