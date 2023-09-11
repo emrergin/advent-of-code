484920fab38748bf4c2066606e8e5d7a05b2d3a9
Emre Ergin <emrergin2757@yahoo.com>
2023-09-11 23:36:28 +0300

    2022 d17p2

--- 2022/day17.js
+++ 2022/day17.js
@@ -1,269 +1,192 @@
 import { readFileSync } from "fs";
-const commands = readFileSync(`day17test.txt`, "utf-8")
+// const commands = readFileSync(`day17test.txt`, "utf-8")
+const commands = readFileSync(`day17.txt`, "utf-8")
   .split("")
   .map((a) => (a === ">" ? 1 : 0));
-// const commands = readFileSync(`day17input.txt`, "utf-8").split("").map(a=>a===">"?1:0);
 const inputLength = commands.length;
-// 1 to right 0 to left
 
 class Board {
   constructor(dimensions) {
     this.dimensions = dimensions;
     this.lines = [];
-    this.createLine(false);
-    this.createLine();
-    this.createLine();
-    this.createLine();
+    this.numberOfPieces = 0;
+    this.windIndex = 0;
+    this.height = 0;
+    this.cells = new Set();
+    this.cells.add(`${0}-${0}`);
+    this.cells.add(`${1}-${0}`);
+    this.cells.add(`${2}-${0}`);
+    this.cells.add(`${3}-${0}`);
+    this.cells.add(`${4}-${0}`);
+    this.cells.add(`${5}-${0}`);
+    this.cells.add(`${6}-${0}`);
   }
-  createLine(isEmpty = true, piece) {
-    let newLine;
-    if (isEmpty) {
-      newLine = ("#" + ".".repeat(this.dimensions - 2) + "#").split("");
-    } else {
-      if (piece === undefined) {
-        newLine = "#".repeat(this.dimensions).split("");
-      } else {
-        newLine = piece.split("");
-      }
-    }
-    this.lines.push(newLine);
-  }
-  addPiece(pieceNo) {
-    switch (pieceNo) {
+}
+
+class Piece {
+  constructor(board) {
+    this.board = board;
+    const pieceType = (board.numberOfPieces % 5) + 1;
+
+    this.cells = [];
+    const startingHeight = board.height + 4;
+    switch (pieceType) {
       case 1:
-        this.createLine(false, "#..@@@@.#");
+        this.cells.push(new Cell(2, startingHeight + 1));
+        this.cells.push(new Cell(3, startingHeight + 1));
+        this.cells.push(new Cell(4, startingHeight + 1));
+        this.cells.push(new Cell(5, startingHeight + 1));
         break;
       case 2:
-        this.createLine(false, "#...@...#");
-        this.createLine(false, "#..@@@..#");
-        this.createLine(false, "#...@...#");
+        this.cells.push(new Cell(3, startingHeight + 3));
+        this.cells.push(new Cell(2, startingHeight + 2));
+        this.cells.push(new Cell(3, startingHeight + 2));
+        this.cells.push(new Cell(4, startingHeight + 2));
+        this.cells.push(new Cell(3, startingHeight + 1));
         break;
       case 3:
-        this.createLine(false, "#..@@@..#");
-        this.createLine(false, "#....@..#");
-        this.createLine(false, "#....@..#");
+        this.cells.push(new Cell(4, startingHeight + 3));
+        this.cells.push(new Cell(4, startingHeight + 2));
+        this.cells.push(new Cell(4, startingHeight + 1));
+        this.cells.push(new Cell(3, startingHeight + 1));
+        this.cells.push(new Cell(2, startingHeight + 1));
         break;
       case 4:
-        this.createLine(false, "#..@....#");
-        this.createLine(false, "#..@....#");
-        this.createLine(false, "#..@....#");
-        this.createLine(false, "#..@....#");
+        this.cells.push(new Cell(2, startingHeight + 4));
+        this.cells.push(new Cell(2, startingHeight + 3));
+        this.cells.push(new Cell(2, startingHeight + 2));
+        this.cells.push(new Cell(2, startingHeight + 1));
         break;
       case 5:
-        this.createLine(false, "#..@@...#");
-        this.createLine(false, "#..@@...#");
+        this.cells.push(new Cell(2, startingHeight + 2));
+        this.cells.push(new Cell(3, startingHeight + 2));
+        this.cells.push(new Cell(2, startingHeight + 1));
+        this.cells.push(new Cell(3, startingHeight + 1));
         break;
       default:
         break;
     }
+    this.fallDown();
   }
-  gravity() {
-    let numberOfLines = this.lines.length;
-    let numberOfColumns = this.dimensions;
-    let continueFalling = true;
-
-    outerloop: for (let i = 0; i < numberOfLines; i++) {
-      for (let j = 0; j < numberOfColumns; j++) {
-        if (this.lines[i][j] === "@") {
-          if (this.lines[i - 1][j] === "#") {
-            continueFalling = false;
-            break outerloop;
-          }
-        }
+  fallDown() {
+    const stop = this.cells.some((cell) =>
+      this.board.cells.has(`${cell.x}-${cell.y - 1}`)
+    );
+    if (stop) {
+      this.joinToBoard();
+    } else {
+      for (let cell of this.cells) {
+        cell.y = cell.y - 1;
       }
+      this.slide(this.board.windIndex);
     }
-
-    for (let i = 0; i < numberOfLines; i++) {
-      if (continueFalling) {
-        for (let j = 0; j < numberOfColumns; j++) {
-          if (this.lines[i][j] === "@") {
-            this.lines[i - 1][j] = "@";
-            this.lines[i][j] = ".";
-          }
-        }
-      } else {
-        for (let j = 0; j < numberOfColumns; j++) {
-          if (this.lines[i][j] === "@") {
-            this.lines[i][j] = "#";
-          }
-        }
-      }
-    }
-    return continueFalling;
   }
-  wind(w) {
-    let numberOfLines = this.lines.length;
-    let numberOfColumns = this.dimensions;
+  slide(windIndex) {
+    const wind = commands[windIndex % inputLength];
+    if (wind === 1) {
+      const stop = this.cells.some(
+        (cell) =>
+          cell.x === this.board.dimensions - 1 ||
+          this.board.cells.has(`${cell.x + 1}-${cell.y}`)
+      );
 
-    if (w === 1) {
-      for (let i = 0; i < numberOfLines; i++) {
-        for (let j = numberOfColumns - 1; j >= 0; j--) {
-          if (this.lines[i][j] === "@" && this.lines[i][j + 1] === "#") {
-            return;
-          }
-        }
-      }
-
-      for (let i = 0; i < numberOfLines; i++) {
-        for (let j = numberOfColumns - 1; j >= 0; j--) {
-          if (this.lines[i][j] === "@") {
-            let temp = this.lines[i][j + 1];
-            this.lines[i][j + 1] = this.lines[i][j];
-            this.lines[i][j] = temp;
-          }
+      if (!stop) {
+        for (let cell of this.cells) {
+          cell.x = cell.x + 1;
         }
       }
     } else {
-      for (let i = 0; i < numberOfLines; i++) {
-        for (let j = 0; j < numberOfColumns; j++) {
-          if (this.lines[i][j] === "@" && this.lines[i][j - 1] === "#") {
-            return;
-          }
-        }
-      }
+      const stop = this.cells.some(
+        (cell) =>
+          cell.x === 0 || this.board.cells.has(`${cell.x - 1}-${cell.y}`)
+      );
 
-      for (let i = 0; i < numberOfLines; i++) {
-        for (let j = 0; j < numberOfColumns; j++) {
-          if (this.lines[i][j] === "@") {
-            let temp = this.lines[i][j - 1];
-            this.lines[i][j - 1] = this.lines[i][j];
-            this.lines[i][j] = temp;
-          }
+      if (!stop) {
+        for (let cell of this.cells) {
+          cell.x = cell.x - 1;
         }
       }
     }
+
+    this.board.windIndex++;
+    this.fallDown();
   }
-  countEmptyLines() {
-    return this.lines.filter((a) => a.join("") === "#.......#").length;
+  joinToBoard() {
+    for (let cell of this.cells) {
+      this.board.cells.add(`${cell.x}-${cell.y}`);
+
+      if (cell.y > this.board.height) {
+        this.board.height = cell.y;
+      }
+    }
+
+    this.board.numberOfPieces++;
   }
-  removeLinesFromBeginning() {
-    let emptyLines = this.countEmptyLines();
-    while (emptyLines > 3) {
-      this.lines.pop();
-      emptyLines--;
+}
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+function addNPieces(n) {
+  const board = new Board(7);
+  while (board.numberOfPieces < n) {
+    new Piece(board);
+  }
+  console.log(board.height);
+}
+
+function catchCyclesWhileAdding(n) {
+  const board = new Board(7);
+  const lengths = [];
+  while (board.numberOfPieces < n) {
+    const prevHeight = board.height;
+    new Piece(board);
+    lengths.push(board.height - prevHeight);
+    const cycle = catchCycle(lengths);
+    if (cycle.start) {
+      console.log(
+        lengths.slice(0, cycle.start).reduce((acc, curr) => acc + curr) +
+          lengths
+            .slice(cycle.start, cycle.length + cycle.start)
+            .reduce((acc, curr) => acc + curr) *
+            Math.floor(n / cycle.length) +
+          lengths
+            .slice(cycle.start, cycle.start + (n % cycle.length) - cycle.start)
+            .reduce((acc, curr) => acc + curr, 0)
+      );
+      break;
     }
   }
-  print() {
-    console.log(
-      this.lines
-        .reverse()
-        .map((a) => a.join(""))
-        .join("\n")
-    );
+}
+
+function catchCycle(array) {
+  const len = array.length;
+  outer: for (let k = 0; k < len / 3; k++) {
+    if ((len - k) % 2 !== 0) {
+      continue outer;
+    }
+    for (let i = k; i < (len + k) / 2; i++) {
+      const j = (len - k) / 2 + i;
+      if (array[i] !== array[j]) {
+        continue outer;
+      }
+    }
+    return { length: (len - k) / 2, start: k };
   }
+  return { length: 0, start: null };
 }
 
 function partOne() {
-  let board = new Board(9);
-
-  let numberOfPiecesSoFar = 0;
-  let windIndex = 0;
-
-  while (numberOfPiecesSoFar < 2022) {
-    let isFalling = true;
-    board.addPiece((numberOfPiecesSoFar % 5) + 1);
-    numberOfPiecesSoFar++;
-    while (isFalling) {
-      board.wind(commands[windIndex % inputLength]);
-      windIndex++;
-      isFalling = board.gravity();
-    }
-    board.removeLinesFromBeginning();
-  }
-  console.log(board.lines.length - 4);
+  addNPieces(2022);
 }
+partOne();
 
 function partTwo() {
-  let board = new Board(9);
-
-  let numberOfPiecesSoFar = 0;
-  let windIndex = 0;
-  let heightsAfterPieces = [0];
-
-  while (!longestRepeatingSubarray(heightsAfterPieces)) {
-    let isFalling = true;
-    board.addPiece((numberOfPiecesSoFar % 5) + 1);
-    numberOfPiecesSoFar++;
-    while (isFalling) {
-      board.wind(commands[windIndex % inputLength]);
-      windIndex++;
-      isFalling = board.gravity();
-    }
-    board.removeLinesFromBeginning();
-    let previousCell = heightsAfterPieces[heightsAfterPieces.length - 1];
-    // console.log(previousCell,board.lines.length-4);
-    heightsAfterPieces.push(board.lines.length - 4 - previousCell);
-    // console.log(heightsAfterPieces.slice(-3))
-  }
-  // while(!patternFound(heightsAfterPieces)){
-  //     heightsAfterPieces.shift();
-  // }
-  console.log(heightsAfterPieces.length);
-
-  console.log(board.lines.length - 4, numberOfPiecesSoFar);
-  console.log(
-    Math.floor(1000000000000 / numberOfPiecesSoFar) * board.lines.length - 4
-  );
+  catchCyclesWhileAdding(1000000000000);
 }
 
-partOne();
-// partTwo();
-
-const exampleArray = [5, 6, 7, 1, 2, 3, 4, 1, 2, 3, 4];
-// function longestRepeatingSubarray(array){
-
-// }
-
-function longestRepeatingSubarray(a) {
-  let n = a.length;
-  let LCSRe = new Array(n + 1);
-  for (let i = 0; i < n + 1; i++) {
-    LCSRe[i] = new Array(n + 1);
-  }
-  for (let i = 0; i < n + 1; i++) {
-    for (let j = 0; j < n + 1; j++) {
-      LCSRe[i][j] = 0;
-    }
-  }
-
-  let res = []; // To store result
-  let res_length = 0; // To store length of result
-
-  // building table in bottom-up manner
-  let i,
-    index = 0;
-  for (i = 1; i <= n; i++) {
-    for (let j = i + 1; j <= n; j++) {
-      // (j-i) > LCSRe[i-1][j-1] to remove
-      // overlapping
-      if (a[i - 1] == a[j - 1] && LCSRe[i - 1][j - 1] < j - i) {
-        LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1;
-
-        // updating maximum length of the
-        // substring and updating the finishing
-        // index of the suffix
-        if (LCSRe[i][j] > res_length) {
-          res_length = LCSRe[i][j];
-          index = Math.max(i, index);
-        }
-      } else {
-        LCSRe[i][j] = 0;
-      }
-    }
-  }
-
-  // If we have non-empty result, then insert all
-  // characters from first character to last
-  // character of String
-  // if (res_length > 0) {
-  //     for (i = index - res_length + 1; i <= index; i++) {
-  //         res.push(a[i - 1]);
-  //     }
-  // }
-
-  // return res;
-  console.log(res_length, a.length);
-  return res_length > a.length / 3;
-}
-
-// console.log(longestRepeatingSubarray(exampleArray))
+partTwo();
