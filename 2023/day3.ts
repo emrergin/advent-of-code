1547e24f43257464e2ef0eedf0be1c8989ce2460
Emre Ergin <emrergin2757@yahoo.com>
2023-12-03 15:18:39 +0300

    2023 d3p2

--- 2023/day3.ts
+++ 2023/day3.ts
@@ -1,53 +1,106 @@
 import { readFileSync } from "fs";
-// const commands = readFileSync(`sasas.txt`, "utf-8").split("\r\n");
 const commands = readFileSync(`day3input.txt`, "utf-8").split("\r\n");
-// .slice(31, 35);
 
 const regex = /([^\d]\d)|(^\d)/g;
 
-let total = 0;
-for (let i = 0; i < commands.length; i++) {
-  const command = commands[i];
-  const allIndicesOfNumberStarts = [...command.matchAll(regex)].map(
-    (a) => a.index
-  );
-  for (let j = 0; j < allIndicesOfNumberStarts.length; j++) {
-    if (checkIfCounted(i, allIndicesOfNumberStarts[j] as number)) {
-      const valueToAdd = getNumberStartingFromIndex(
-        i,
-        allIndicesOfNumberStarts[j] as number
-      );
-      // console.log(valueToAdd);
-      total += valueToAdd;
+function partOne() {
+  let total = 0;
+  for (let i = 0; i < commands.length; i++) {
+    const command = commands[i];
+    const allIndicesOfNumberStarts = [...command.matchAll(regex)].map(
+      (a) => a.index
+    );
+    for (let j = 0; j < allIndicesOfNumberStarts.length; j++) {
+      if (checkIfCounted(i, allIndicesOfNumberStarts[j] as number)) {
+        const valueToAdd = getNumberStartingFromIndex(
+          i,
+          allIndicesOfNumberStarts[j] as number
+        );
+        total += valueToAdd;
+      }
     }
   }
-}
-console.log(total);
+  console.log(total);
 
-function checkIfCounted(yCor: number, index: number) {
-  let currentX = index;
-  let lassPass = 0;
-  while (currentX < commands[0].length && lassPass < 2) {
-    for (let i = -1; i <= 1; i++) {
-      if (yCor + i < 0) {
-        continue;
+  function checkIfCounted(yCor: number, index: number) {
+    let currentX = index;
+    let lassPass = 0;
+    while (currentX < commands[0].length && lassPass < 2) {
+      for (let i = -1; i <= 1; i++) {
+        if (yCor + i < 0) {
+          continue;
+        }
+        if (yCor + i >= commands.length) {
+          continue;
+        }
+        const currentChar = commands[yCor + i][currentX];
+        if (/[^\d\.]/.test(currentChar)) {
+          return true;
+        }
       }
-      if (yCor + i >= commands.length) {
-        continue;
+      if (/[^\d]/.test(commands[yCor][currentX])) {
+        lassPass++;
       }
-      const currentChar = commands[yCor + i][currentX];
-      if (/[^\d\.]/.test(currentChar)) {
-        console.log(currentChar);
-        return true;
-      }
+      currentX++;
     }
-    if (/[^\d]/.test(commands[yCor][currentX])) {
-      lassPass++;
-    }
-    currentX++;
+    return false;
   }
-  return false;
 }
+
+function partTwo() {
+  const allGears = new Map();
+  for (let i = 0; i < commands.length; i++) {
+    const command = commands[i];
+    const allIndicesOfNumberStarts = [...command.matchAll(regex)].map(
+      (a) => a.index
+    );
+    for (let j = 0; j < allIndicesOfNumberStarts.length; j++) {
+      checkIfCounted(i, allIndicesOfNumberStarts[j] as number);
+    }
+  }
+  let total = 0;
+  allGears.forEach((g) => {
+    if (g.length === 2) {
+      total += g[0] * g[1];
+    }
+  });
+  console.log(total);
+
+  function checkIfCounted(yCor: number, index: number) {
+    let currentX = index;
+    let lassPass = 0;
+    while (currentX < commands[0].length && lassPass < 2) {
+      for (let i = -1; i <= 1; i++) {
+        if (yCor + i < 0) {
+          continue;
+        }
+        if (yCor + i >= commands.length) {
+          continue;
+        }
+        const currentChar = commands[yCor + i][currentX];
+        if (/\*/.test(currentChar)) {
+          const relevantGear = allGears.get(`${yCor + i}-${currentX}`);
+          if (relevantGear) {
+            relevantGear.push(getNumberStartingFromIndex(yCor, index));
+          } else {
+            allGears.set(`${yCor + i}-${currentX}`, [
+              getNumberStartingFromIndex(yCor, index),
+            ]);
+          }
+          return true;
+        }
+      }
+      if (/[^\d]/.test(commands[yCor][currentX])) {
+        lassPass++;
+      }
+      currentX++;
+    }
+    return false;
+  }
+}
+
+partOne();
+partTwo();
 
 function getNumberStartingFromIndex(yCor: number, index: number) {
   let currentX = index;
@@ -59,6 +112,5 @@ function getNumberStartingFromIndex(yCor: number, index: number) {
     resultText += commands[yCor][currentX];
     currentX++;
   }
-  console.log(parseInt(resultText, 10));
   return parseInt(resultText, 10);
 }
