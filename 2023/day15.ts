4f0a19e68e569f6f759e5002c7acd3d8512924eb
Emre Ergin <emrergin2757@yahoo.com>
2024-01-09 10:39:51 +0300

    2023 d15p2

--- 2023/day15.ts
+++ 2023/day15.ts
@@ -20,4 +20,50 @@ function partOne() {
   console.log(total);
 }
 
-partOne();
+// partOne();
+
+function partTwo() {
+  let arr: { label: string; fLen: number }[][] = [...Array(256)].map((e) => []);
+  for (let command of commands) {
+    const parsedCommand = parseCommand(command);
+    const targetBox = hash(parsedCommand.label);
+    if (parsedCommand.sign === "-") {
+      const targetIndex = arr[targetBox].findIndex(
+        (l) => l.label === parsedCommand.label
+      );
+      if (targetIndex !== -1) {
+        arr[targetBox].splice(targetIndex, 1);
+      }
+    } else if (parsedCommand.fLen) {
+      const targetLens = arr[targetBox].find(
+        (l) => l.label === parsedCommand.label
+      );
+      if (targetLens) {
+        targetLens.fLen = parsedCommand.fLen;
+      } else {
+        arr[targetBox].push({
+          label: parsedCommand.label,
+          fLen: parsedCommand.fLen,
+        });
+      }
+    }
+  }
+  let total = 0;
+  for (let i = 0; i < 256; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      total += (i + 1) * (j + 1) * arr[i][j].fLen;
+    }
+  }
+  console.log(total);
+}
+partTwo();
+
+function parseCommand(str: string) {
+  const regex = /(\w+)(=|-)(\d)?/;
+  const res = str.match(regex);
+  return {
+    label: res?.[1] as string,
+    sign: res?.[2] as "-" | "=",
+    fLen: Number(res?.[3]),
+  };
+}
