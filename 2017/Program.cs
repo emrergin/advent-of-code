0ac23224c1306affbd6ea022e0dc664352817eb7
Emre Ergin <emrergin2757@yahoo.com>
2024-06-05 18:49:47 +0300

    2017 d9p1

--- 2017/Program.cs
+++ 2017/Program.cs
@@ -4,5 +4,4 @@ _2017.day1.Part2.Solve();
 _2017.day2.Part1.Solve();
 _2017.day2.Part2.Solve();*/
 
-_2017.day8.Part1.Solve();
-_2017.day8.Part2.Solve();
+_2017.day9.Part1.Solve();
---
+++ 2017/day9/Part1.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _2017.day9
+{
+    internal class Part1
+    {
+        internal static Group[] allGroups = [];
+        internal static Stack<Group> groupStack = [];
+        public static void AddChild(Group group)
+        {
+            var newGroup = new Group(group.score + 1);
+            allGroups = [.. allGroups, newGroup];
+            groupStack.Push(newGroup);
+        }
+        internal class Group(int score)
+        {
+            internal int score = score;
+        }
+        public static void Solve()
+        {
+            var line = Utilities.ReadLine(9);
+            string cleanedString = Regex.Replace(line, @"\!.", "");
+            cleanedString = Regex.Replace(cleanedString, @"\<.*?\>", "");
+            var outestGroup = new Group(1);
+            allGroups = [outestGroup];
+            groupStack.Push(outestGroup);
+            int strIndex = 1;
+
+            while (groupStack.Count > 0)
+            {
+                var currentGroup = groupStack.Peek();
+
+                if (cleanedString[strIndex] == '{')
+                {
+                    AddChild(currentGroup);
+                }
+                else if (cleanedString[strIndex] == '}')
+                {
+                    groupStack.Pop();
+                }
+                strIndex++;
+            }
+
+            Console.WriteLine(allGroups.Select(i => i.score).Sum());
+
+        }
+    }
+}
