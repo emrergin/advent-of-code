a2791810251344a521c8e2bda98658909f3d11da
Emre Ergin <emrergin2757@yahoo.com>
2024-07-04 14:37:35 +0300

    2017 d16 p2

--- 2017/day16/Part2.cs
+++ 2017/day16/Part2.cs
@@ -8,23 +8,67 @@ namespace _2017.day16
 {
     internal class Part2
     {
+        internal static Cycle CatchCycle(string[] array)
+        {
+            int len = array.Length;
+
+            for (int k = 0; k < len / 3; k++)
+            {
+                if ((len - k) % 2 != 0)
+                {
+                    continue;
+                }
+                for (int i = k; i < (len + k) / 2; i++)
+                {
+                    int j = (len - k) / 2 + i;
+                    if (array[i] != array[j])
+                    {
+                        goto Outer;
+                    }
+                }
+                return new Cycle((len - k) / 2, k);
+
+            Outer:
+                {
+                }
+            }
+            return new Cycle(0, null);
+
+        }
+        internal class Cycle(int length, int? start)
+        {
+            public readonly int length = length;
+            public readonly int? start = start;
+        }
         public static void Solve()
         {
             string[] commands = Utilities.ReadArrayOfStrings(16)[0][0].Split(",");
             char[] letters = "abcdefghijklmnop".ToCharArray();
+            string[] standingsSoFar = [];
+            int i;
+            Cycle? existingCycle = null;
 
-            for (int i = 0; i < 1000000000; i++)
+
+            for (i = 0; i < 1000000000; i++)
             {
                 foreach (string command in commands)
                 {
                     letters = Part1.OnePass(letters, command);
                 }
-                if (i % 1000 == 0)
+                standingsSoFar = [.. standingsSoFar, String.Join("", letters)];
+                existingCycle = CatchCycle(standingsSoFar);
+                if (existingCycle.start != null)
                 {
-                    Console.WriteLine(i);
+                    goto End;
                 }
             }
-            Console.WriteLine(String.Join("", letters));
+        End:
+            {
+                if (existingCycle != null && existingCycle.start != null)
+                {
+                    Console.WriteLine(standingsSoFar[(1000000000 %  existingCycle.length) + (int)existingCycle.start-1]);
+                }
+            }
         }
     }
 }
