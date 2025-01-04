2299202c4e47dbcc7bedadfa6e7ca618277a5a81
Emre Ergin <emrergin2757@yahoo.com>
2025-01-05 02:17:21 +0300

    2024 d15 p2

--- 2024/day15.ts
+++ 2024/day15.ts
@@ -17,12 +17,45 @@ class Cell {
   y: number;
   static gridMap: Map<string, Cell>;
   static part: 1 | 2;
+  static cellstatwillMove: { cell: Cell; orderingAxis: number }[];
 
   constructor(type: "box" | "wall", x: number, y: number) {
     this.type = type;
     this.x = x;
     this.y = y;
     Cell.gridMap.set(`${x}|${y}`, this);
+  }
+
+  static moveAll(direction: "^" | "v" | "<" | ">") {
+    let allCells = new Set<string>();
+    Cell.cellstatwillMove = Cell.cellstatwillMove.reduce((acc, curr) => {
+      if (!allCells.has(`${curr.cell.x}|${curr.cell.y}`)) {
+        acc.push(curr);
+        allCells.add(`${curr.cell.x}|${curr.cell.y}`);
+      }
+      return acc;
+    }, [] as typeof Cell.cellstatwillMove);
+
+    if (direction === "^" || direction === "<") {
+      Cell.cellstatwillMove.sort((a, b) => a.orderingAxis - b.orderingAxis);
+    } else {
+      Cell.cellstatwillMove.sort((a, b) => b.orderingAxis - a.orderingAxis);
+    }
+
+    for (let { cell } of this.cellstatwillMove) {
+      cell.moveSelf(direction);
+    }
+
+    Cell.cellstatwillMove = [];
+  }
+
+  moveSelf(direction: "^" | "v" | "<" | ">") {
+    let targetX = this.x + directions[direction][0];
+    let targetY = this.y + directions[direction][1];
+    Cell.gridMap.set(`${targetX}|${targetY}`, this);
+    Cell.gridMap.delete(`${this.x}|${this.y}`);
+    this.x = targetX;
+    this.y = targetY;
   }
 
   move(direction: "^" | "v" | "<" | ">") {
@@ -61,16 +94,19 @@ class Cell {
     }
     let noWalls = nextCells.every((c) => c.move(direction));
     if (!noWalls) {
+      Cell.cellstatwillMove = [];
       return false;
     } else {
-      Cell.gridMap.delete(`${this.x}|${this.y}`);
-      let targetX = this.x + directions[direction][0];
-      let targetY = this.y + directions[direction][1];
-      Cell.gridMap.set(`${targetX}|${targetY}`, this);
-      this.x = targetX;
-      this.y = targetY;
+      Cell.cellstatwillMove.push({
+        cell: this,
+        orderingAxis: direction === "^" || direction === "v" ? this.y : this.x,
+      });
       return true;
     }
+  }
+
+  get gps() {
+    return this.type === "wall" ? 0 : 100 * this.y + this.x;
   }
 }
 
@@ -84,6 +120,7 @@ const directions = {
 function partOne() {
   Cell.gridMap = new Map<string, Cell>();
   Cell.part = 1;
+  Cell.cellstatwillMove = [];
 
   let commandsCopy = structuredClone(commands);
   let gridCopy = structuredClone(grid);
@@ -108,6 +145,7 @@ function partOne() {
     let targetY = robotY + directions[commandsCopy[i]][1];
     let targetCell = Cell.gridMap.get(`${targetX}|${targetY}`);
     targetCell?.move(commandsCopy[i]);
+    Cell.moveAll(commandsCopy[i]);
     let newTarget = Cell.gridMap.get(`${targetX}|${targetY}`);
     if (newTarget === undefined) {
       robotX = targetX;
@@ -117,9 +155,7 @@ function partOne() {
 
   let total = 0;
   for (let [, value] of Cell.gridMap) {
-    if (value.type === "box") {
-      total += value.y * 100 + value.x;
-    }
+    total += value.gps;
   }
 
   console.log(total);
@@ -128,6 +164,7 @@ function partOne() {
 function partTwo() {
   Cell.gridMap = new Map<string, Cell>();
   Cell.part = 2;
+  Cell.cellstatwillMove = [];
 
   let commandsCopy = structuredClone(commands);
   let gridCopy = structuredClone(grid);
@@ -135,46 +172,28 @@ function partTwo() {
   let robotY = -1;
 
   for (let i = 0; i < gridCopy.length; i++) {
-    for (let j = 0; j < gridCopy[0].length * 2; j++) {
-      if (j % 2 === 0) {
-        if (gridCopy[i][j / 2] === "#") {
-          new Cell("wall", j, i);
-        } else if (gridCopy[i][j / 2] === "O") {
-          new Cell("box", j, i);
-        } else if (gridCopy[i][j / 2] === "@") {
-          robotX = j;
-          robotY = i;
-        }
+    for (let j = 0; j < gridCopy[0].length; j++) {
+      if (gridCopy[i][j] === "#") {
+        new Cell("wall", j * 2, i);
+      } else if (gridCopy[i][j] === "O") {
+        new Cell("box", j * 2, i);
+      } else if (gridCopy[i][j] === "@") {
+        robotX = j * 2;
+        robotY = i;
       }
     }
   }
 
-  for (let i = 0; i < commandsCopy.length; i++) {
-    let currentDirection = commandsCopy[i];
-    let targetX = robotX + directions[commandsCopy[i]][0];
-    let targetY = robotY + directions[commandsCopy[i]][1];
-    let targetCell: Cell | undefined;
-    if (currentDirection === "^" || currentDirection === "v") {
-      targetCell =
-        Cell.gridMap.get(`${targetX}|${targetY}`) ||
-        Cell.gridMap.get(`${targetX - 1}|${targetY}`);
-    } else if (currentDirection === "<") {
-      targetCell = Cell.gridMap.get(`${targetX - 1}|${targetY}`);
-    } else {
-      targetCell = Cell.gridMap.get(`${targetX}|${targetY}`);
-    }
+  for (let currentDirection of commandsCopy) {
+    let targetX = robotX + directions[currentDirection][0];
+    let targetY = robotY + directions[currentDirection][1];
+    let targetCell = getTarget(targetX, targetY, currentDirection);
 
-    targetCell?.move(commandsCopy[i]);
-    let newTarget: Cell | undefined;
-    if (currentDirection === "^" || currentDirection === "v") {
-      newTarget =
-        Cell.gridMap.get(`${targetX}|${targetY}`) ||
-        Cell.gridMap.get(`${targetX - 1}|${targetY}`);
-    } else if (currentDirection === "<") {
-      newTarget = Cell.gridMap.get(`${targetX - 1}|${targetY}`);
-    } else {
-      newTarget = Cell.gridMap.get(`${targetX}|${targetY}`);
+    if (targetCell) {
+      targetCell.move(currentDirection);
+      Cell.moveAll(currentDirection);
     }
+    let newTarget = getTarget(targetX, targetY, currentDirection);
 
     if (newTarget === undefined) {
       robotX = targetX;
@@ -182,11 +201,22 @@ function partTwo() {
     }
   }
 
+  function getTarget(tx: number, ty: number, direction: "^" | "v" | "<" | ">") {
+    let target: Cell | undefined;
+    if (direction === "^" || direction === "v") {
+      target =
+        Cell.gridMap.get(`${tx}|${ty}`) || Cell.gridMap.get(`${tx - 1}|${ty}`);
+    } else if (direction === "<") {
+      target = Cell.gridMap.get(`${tx - 1}|${ty}`);
+    } else {
+      target = Cell.gridMap.get(`${tx}|${ty}`);
+    }
+    return target;
+  }
+
   let total = 0;
   for (let [, value] of Cell.gridMap) {
-    if (value.type === "box") {
-      total += value.y * 100 + value.x;
-    }
+    total += value.gps;
   }
 
   console.log(total);
